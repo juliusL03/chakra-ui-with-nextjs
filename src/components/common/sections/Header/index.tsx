c755b9d4860320814dcbcc1b25c9fc7216daac4e
julius legaspi <julius.legaspi@yachtly.com.sg>
2024-02-19 12:37:46 +0800

    fixed the scroll event

--- src/components/common/sections/Header/index.tsx
+++ src/components/common/sections/Header/index.tsx
@@ -1,10 +1,24 @@
 import {clsx} from 'clsx'
 import styles from './header.module.scss'
 import { Button } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 
 const Header: React.FC = () => {
+ const [navbar, setNavBar] = useState(false)
+
+ const changeBackground = () => {
+  if(window.scrollY >= 50) {
+   setNavBar(true)
+  } else {
+   setNavBar(false)
+  }
+ }
+
+ useEffect( () => {
+  window.addEventListener('scroll', changeBackground)
+ }, [])
 	return (
-		<div className={clsx(styles.container)}>
+		<div className={clsx(styles.container, navbar ? styles['active'] : '')}>
 				<h1>JuliusL.</h1>
     <div className={clsx(styles.navList)}>
      <span>About me</span>
